cb41803ba0c2a34cef77c30c59849a9d4c1f523b
Tim Sugaipov <tim@Tims-MacBook-Pro.local>
2020-10-26 10:14:25 +0000

    fixed requirements and group_summary postgres confilct

--- sql/group_summary.sql
+++ sql/group_summary.sql
@@ -1,10 +1,10 @@
 SELECT source
-     , AVG(title_letters)
-     , AVG(title_words)
-     , AVG(title_sentences)
-     , AVG(summary_letters)
-     , AVG(summary_words)
-     , AVG(summary_sentences)
+     , AVG(title_letters) AS average_tl
+     , AVG(title_words) AS average_tw
+     , AVG(title_sentences) AS average_ts
+     , AVG(summary_letters) AS average_sl
+     , AVG(summary_words) AS average_sw
+     , AVG(summary_sentences) AS average_sss
 
 FROM {{user_prefix}}{{table}}
 
